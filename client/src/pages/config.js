399ed11513ef321c9decd09e179bb463ebf370b6
Ajay kumar sankula <100196051+Ajaykumarsankula@users.noreply.github.com>
2026-01-10 12:04:37 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://107.22.149.95:84"   
+const API_BASE_URL = "http://a55bcd4c854e04d0aaab0517807308f0-617522041.us-west-2.elb.amazonaws.com"   
 // pubip :port of backend
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // export default API_BASE_URL;
